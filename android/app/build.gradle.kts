0ac9ba1afef6645e46cdb41333706cad0fb6d4ae
Aranda Vallejos, Oscar Gabriel <u202218167@upc.edu.pe>
2025-07-09 11:44:25 -0500

    feat: add R8 for android app optimization

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -44,8 +44,10 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = false
-            isShrinkResources = false
+            // Enables code-related app optimization.
+            isMinifyEnabled = true
+            // Enables resource shrinking.
+            isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
