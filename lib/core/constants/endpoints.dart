30043ee698e3dc92ac48aa5d4cefcfecbe6e24c0
Myck <136953823+Kmykh@users.noreply.github.com>
2025-10-08 17:08:32 -0500

    Update API baseUrl in endpoints constants
    Changed the baseUrl in Endpoints to point to the new backend API hosted in brazilsouth Azure region. This ensures all requests use the updated service endpoint.

--- lib/core/constants/endpoints.dart
+++ lib/core/constants/endpoints.dart
@@ -1,5 +1,5 @@
 class Endpoints {
-  static const String baseUrl = 'https://vacapp-api-platform-app-202.jollypebble-386b7d46.westus3.azurecontainerapps.io/api/v1';
+  static const String baseUrl = 'https://vacappexpbackend-cahacmh4atbxd0g3.brazilsouth-01.azurewebsites.net/api/v1';
   static const String login = '$baseUrl/user/sign-in';
   static const String register = '$baseUrl/user/sign-up';
   static const String animal = '$baseUrl/bovines';
